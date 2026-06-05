9363c21f83db108c70fc896c9f1c58ba22ad8393
Tushar Sakhamuri <tusharsakhamuri9@gmail.com>
2026-06-05 21:55:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
 API_KEY = os.getenv("GOOGLE_API_KEY")
+
+if not API_KEY:
+    API_KEY = st.secrets["GOOGLE_API_KEY"]
